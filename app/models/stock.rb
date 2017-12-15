5a243048082700cd213784904746096e925252b2
Sarah Basinger <sarrey@gmail.com>
2017-12-15 15:48:12 -0500

    WIP adding stock controller and model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,7 @@
-class Stock < ApplicationRecord
+class Stock < ActiveRecord::Base
+
+	def self.new_from_lookup(ticker_symbol)
+		looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+		new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.l)
+	end
 end
