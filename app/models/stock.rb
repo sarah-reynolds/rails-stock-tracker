93ed2fc8da98d0c532f401f26eed00348f7caee8
Sarah Basinger <sarrey@gmail.com>
2018-01-05 12:03:07 -0500

    Display stock results on my_portfolio page upon search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,11 @@ class Stock < ActiveRecord::Base
 
 	def self.new_from_lookup(ticker_symbol)
 		looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-		new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.l)
+		price = strip_commas(looked_up_stock.l)
+		new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+	end
+
+	def self.strip_commas(number)
+		number.gsub(",","")
 	end
 end
