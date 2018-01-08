def001115caa38d975e5b939bcaab955b60d7b39
Sarah Basinger <sarrey@gmail.com>
2018-01-07 20:34:16 -0500

    WIP friend search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
 	end
 
 	def my_friends
+		@friendships = current_user.friends
+	end
 
+	def search
+		if params[:search_params].blank?
+			flash.now[:danger] = "You have entered an empty search string"
+		else	
+			@users = User.search(params[:search_params])
+			flash.now[:danger] = "No results" if @users.blank?
+		end
+		respond_to do |format|
+			format.js { render partial: 'friends/result'}
+		end
 	end
 end
